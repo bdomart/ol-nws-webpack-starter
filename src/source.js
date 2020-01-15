272e7a0ed95322c129787cd602ccde3c1c637a30
bdomart <bdomart@normandiewebschool.fr>
2020-01-15 11:15:22 +0100

    Ajax features call added

--- src/source.js
+++ src/source.js
@@ -1,6 +1,5 @@
 import './source.scss';
 import 'ol/ol.css';
-import Feature from 'ol/Feature';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import Overlay from 'ol/Overlay';
@@ -9,6 +8,7 @@ import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 import {OSM, Vector as VectorSource} from 'ol/source';
 import {Circle as CircleStyle, Fill, Stroke, Style, Text} from 'ol/style';
 import {transform} from 'ol/proj';
+import $ from 'jquery';
 
 var imagePoint = new CircleStyle({
     radius: 3.5,
@@ -51,39 +51,6 @@ var styles = {
     })
 };
 
-var styleFunction = function (feature) {
-    return styles[feature.getGeometry().getType()];
-};
-
-function convertCoordinates(x, y) {
-    return transform([x, y], 'EPSG:4326', 'EPSG:3857')
-}
-
-var features = require("../features.json"); //A remplacer par un appel API
-features.forEach(function (feature) {
-    feature.geometry.coordinates = convertCoordinates(feature.geometry.coordinates[0], feature.geometry.coordinates[1])
-});
-
-var geojsonObject = {
-    'type': 'FeatureCollection',
-    'crs': {
-        'type': 'name',
-        'properties': {
-            'name': 'EPSG:3857'
-        }
-    },
-    'features': features
-};
-
-var vectorSource = new VectorSource({
-    features: (new GeoJSON()).readFeatures(geojsonObject)
-});
-
-var vectorLayer = new VectorLayer({
-    source: vectorSource,
-    style: styleFunction
-});
-
 /**
  * Elements that make up the popup.
  */
@@ -112,43 +79,89 @@ closer.onclick = function () {
     return false;
 };
 
-var map = new Map({
-    layers: [
-        new TileLayer({
-            source: new OSM()
-        }),
-        vectorLayer
-    ],
-    overlays: [overlay],
-    target: 'carteNWS',
-    view: new View({
-        center: convertCoordinates(1.066530, 49.428470),
-        zoom: 12
-    })
-});
+var styleFunction = function (feature) {
+    return styles[feature.getGeometry().getType()];
+};
 
-var selected = null;
-map.on('pointermove', function (e) {
-    if (selected !== null) {
-        selected.setStyle(undefined);
-        selected = null;
+function convertCoordinates(x, y) {
+    return transform([x, y], 'EPSG:4326', 'EPSG:3857')
+}
+
+$.ajax({
+    url: 'http://localhost:8001/geojson',
+    type: 'GET',
+    success: function (response) {
+        var features = response;
+
+        features.forEach(function (feature) {
+            feature.geometry.coordinates = convertCoordinates(feature.geometry.coordinates[0], feature.geometry.coordinates[1]);
+        });
+
+        var geojsonObject = {
+            'type': 'FeatureCollection',
+            'crs': {
+                'type': 'name',
+                'properties': {
+                    'name': 'EPSG:3857'
+                }
+            },
+            'features': features
+        };
+
+        var vectorSource = new VectorSource({
+            features: new GeoJSON().readFeatures(geojsonObject)
+        });
+
+        var vectorLayer = new VectorLayer({
+            source: vectorSource,
+            style: styleFunction
+        });
+
+        var map = new Map({
+            layers: [
+                new TileLayer({
+                    source: new OSM()
+                }),
+                vectorLayer
+            ],
+            overlays: [overlay],
+            target: 'carteNWS',
+            view: new View({
+                center: convertCoordinates(1.066530, 49.428470),
+                zoom: 12
+            })
+        });
+
+        var selected = null;
+        map.on('pointermove', function (e) {
+            if (selected !== null) {
+                selected.setStyle(undefined);
+                selected = null;
+            }
+
+            map.forEachFeatureAtPixel(e.pixel, function (f) {
+                selected = f;
+
+                var geometry = f.getGeometry();
+                var style = styles['Highlight' + geometry.getType()];
+
+                var text = f.get('address');
+                var location = f.get('location');
+                if (typeof location != 'undefined') {
+                    text = location[0].name;
+                }
+                style.getText().setText(text);
+                f.setStyle(style);
+                return true;
+            });
+        });
+
+        map.on('singleclick', function (e) {
+            map.forEachFeatureAtPixel(e.pixel, function (f) {
+                var coordinate = e.coordinate;
+                content.innerHTML = '<p>' + f.get('address') + '<br/>' + f.get('postalCode') + ' ' + f.get('city') + '</p>';
+                overlay.setPosition(coordinate);
+            });
+        });
     }
-
-    map.forEachFeatureAtPixel(e.pixel, function (f) {
-        selected = f;
-
-        var geometry = f.getGeometry();
-        var style = styles['Highlight' + geometry.getType()];
-        style.getText().setText(f.get('name'));
-        f.setStyle(style);
-        return true;
-    });
-});
-
-map.on('singleclick', function (e) {
-    map.forEachFeatureAtPixel(e.pixel, function (f) {
-        var coordinate = e.coordinate;
-        content.innerHTML = '<p>' + f.get('name') + '<br/>' + f.get('description') + '</p>';
-        overlay.setPosition(coordinate);
-    });
 });
